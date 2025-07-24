534cbd5a37274c547fab987406e3a91043a9d3a2
Wambui Kibathi <zeldabobo02@gmail.com>
2025-07-24 15:53:19 +0300

    WIP: Add useState hook and logic

--- src/components/Results Page/ResultsPage.jsx
+++ src/components/Results Page/ResultsPage.jsx
@@ -1,4 +1,9 @@
+import { useState } from 'react';
+
 export function ResultsPage() {
+    const [username, setUsername] = useState('');
+    const [error, setError] = useState('');
+    
     return (
         <>
         </>
