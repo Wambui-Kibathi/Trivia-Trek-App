3043d4857499791afa1754ac99f8b294438f4e55
MosingiA <ondicho33@gmail.com>
2025-07-24 17:06:58 +0300

    adding useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
-import {useState} from 'react';
+import { NavBar } from './components/NavBar/NavBar.jsx';
+import { RegisterForm } from './components/Register Form/RegisterForm.jsx';
+import QuizList from './components/Quiz List/QuizList.jsx';
+import QuestionsCard from './components/Questions Card/QuestionsCard.jsx';
+import { GameControls } from './components/Game Controls/GameControls.jsx';
+import { ResultsPage } from './components/Results Page/ResultsPage.jsx';
+import { About } from './components/About/About.jsx';
+import { questionsData } from './data/questions.js';
+import { fetchApiQuestions } from './utils/fetchApiQuestions.js';
+import './App.css'
 
 function App() {
+  const [page, setPage] = useState('register');
+  const [user, setUser] = useState(null);
+  const [selectedQuiz, setSelectedQuiz] = useState(null);
+  const [level, setLevel] = useState(null);
+  const [questions, setQuestions] = useState([]);
+  const [current, setCurrent] = useState(0);
+  const [answers, setAnswers] = useState([]);
+  const [paused, setPaused] = useState(false);
+  const [timeLeft, setTimeLeft] = useState(60);
 
-    return(
-
-        <>
-        </>
-    )
-}
-
-export default App;
+  
